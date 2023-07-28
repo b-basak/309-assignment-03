635bff43a8d984a6585b2260972ac28f9e372a3b
Bonni Basak <126444262+b-basak@users.noreply.github.com>
2023-07-28 15:03:21 +0600

    Add files via upload

---
+++ Assignment03/insert.php
@@ -0,0 +1,34 @@
+<?php
+$localhost='localhost';
+$username='root';
+$password='root03';
+$db='login';
+
+$conn=new mysqli($localhost,$username,$password,$db);
+
+if($conn->connect_error){
+    die("connection Error:".$conn->connect_error);
+}
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $mail=$_POST['mail'];
+    $phone=$_POST['phone'];
+    $msg=$_POST['msg'];
+
+
+
+    $sql="INSERT INTO login (Fname,Lname,Email,phone,massage) VALUES ('$fname','$lname','$mail','$phone','$msg')";
+    $result=$conn->query($sql);
+
+    if($result===TRUE){
+        echo "Login Seccesful";
+
+    }else{
+        echo "ERROR: ".mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+
+?>
---
+++ Assignment03/login.sql
@@ -0,0 +1,9 @@
+CREATE Table login(
+    Id INT(10) AUTO_INCREMENT PRIMARY KEY,
+    Fname VARCHAR(255),
+    Lname VARCHAR(255),
+    Email VARCHAR(255),
+    phone VARCHAR(255),
+    massage VARCHAR(255)
+
+)
